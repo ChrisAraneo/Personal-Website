a9e54f7c5e10dca0b56c96fdabd5bb8f2803d46e
Krzysztof Pająk <chris.araneo@gmail.com>
2019-10-07 19:16:05 +0200

    Fixing mobile scrolls...

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -2,48 +2,43 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { CSSTransition } from 'react-transition-group'
 
-const PageContainer = (props) => {
-    if (props.mobile === false) {
-        return (
-            <div className="page-container">
-                {props.routes.map(({ path, Component }) => (
-                    <Route key={path} exact path={path}>
-                        {({ match }) => (
-                            <CSSTransition
-                                in={match != null}
-                                timeout={1000}
-                                classNames="page"
-                                unmountOnExit>
-                                <div className="page">
-                                    <Component mobile={props.mobile} />
-                                </div>
-                            </CSSTransition>
-                        )}
-                    </Route>
-                ))}
-            </div>
-        );
-    } else {
-        return (
-            <>
-                {props.routes.map(({ path, Component }) => (
-                    <Route key={path} exact path={path}>
-                        {({ match }) => (
-                            <CSSTransition
-                                in={match != null}
-                                timeout={1000}
-                                classNames="page"
-                                unmountOnExit>
-                                <div className="page">
-                                    <Component mobile={props.mobile} />
-                                </div>
-                            </CSSTransition>
-                        )}
-                    </Route>
-                ))}
-            </>
-        );
-    }
-}
+const PageContainer = (props) => (
+    <div className="page-container">
+        {props.routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+                {({ match }) => (
+                    <CSSTransition
+                        in={match != null}
+                        timeout={1000}
+                        classNames="page"
+                        unmountOnExit>
+                        <div className="page">
+                            <Component mobile={props.mobile} />
+                        </div>
+                    </CSSTransition>
+                )}
+            </Route>
+        ))}
+    </div>
+    /*
+    <div className="page-container">
+        {props.routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+                {({ match }) => (
+                    <CSSTransition
+                        in={match != null}
+                        timeout={1000}
+                        classNames="page"
+                        unmountOnExit>
+                        <div className="page">
+                            <Component mobile={props.mobile} />
+                        </div>
+                    </CSSTransition>
+                )}
+            </Route>
+        ))}
+    </div>
+    */
+);
 
 export default PageContainer;
